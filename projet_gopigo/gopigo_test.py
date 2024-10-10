d0ccd3490ec42c49b6e9a27972a3b902ba15f350
flori <161379261+flo7716@users.noreply.github.com>
2024-10-10 15:12:32 +0200

    removed closed eyes

--- projet_gopigo/gopigo_test.py
+++ projet_gopigo/gopigo_test.py
@@ -4,16 +4,15 @@ gpg = EasyGoPiGo3()
 servo = gpg.init_servo()
 my_distance_sensor = gpg.init_distance_sensor()
 servo.rotate_servo(94)
-gpg.close_left_eye()
-gpg.close_right_eye()
 
 
 def main(speed):
     gpg.set_speed(speed)
     gpg.drive_cm(100)
     gpg.set_speed(300)
-    gpg.drive(100)
+    gpg.drive_cm(100)
 
+def obstacle_detection():
 
 
 if __name__ == main:
