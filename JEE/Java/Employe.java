50f7f36db8664a8ead5df2291049aef4dec71896
flori <161379261+flo7716@users.noreply.github.com>
2025-02-10 11:13:14 +0100

    edited comptable and employe classes

---
+++ JEE/Java/Employe.java
@@ -0,0 +1,23 @@
+public class Employe{
+    private String nom, prenom;
+    private double salaire;
+
+    // Constructeurs
+    public Employe() {}
+
+    public Employe(String n, String p){
+        nom = n;
+        prenom = p;
+    }
+
+    public Employe(String n, String p, double s){
+        nom = n;
+        prenom = p;
+        salaire = s;
+    }
+}
+
+
+
+
+
