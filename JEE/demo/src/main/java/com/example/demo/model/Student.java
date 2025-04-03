a443cc1dde0ddcb3bae4fc8cf84d54af3fe9556b
flo7716 <florian.andre.77@gmail.com>
2025-04-03 12:00:52 +0200

    student

--- JEE/demo/src/main/java/com/example/demo/model/Student.java
+++ JEE/demo/src/main/java/com/example/demo/model/Student.java
@@ -1,12 +1,36 @@
-package model;
+package com.example.demo.model;
 import java.time.LocalDate;
 import java.time.Period;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student_table")
+
+
+
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
     private int id;
+
+    
+    @Column(name = "student_name")
     private String name;
+
+    @Column(name = "student_email")
     private String email;
+
+    @Column(name = "student_dob")
     private LocalDate dob;
+
+    @Column(name = "student_age")
     private int age;
 
     public Student(int id, String name, String email, LocalDate dob) {
