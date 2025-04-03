9713e6010330a6397cc6e02b0628baab9a44d0e7
flo7716 <florian.andre.77@gmail.com>
2025-04-03 12:30:50 +0200

    added repository

--- JEE/demo/src/main/java/com/example/demo/service/StudentService.java
+++ JEE/demo/src/main/java/com/example/demo/service/StudentService.java
@@ -1,5 +1,17 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.StudentRepository;
+
+@Service
+
 public class StudentService {
-    
+    @Autowired
+    private final StudentRepository studentrepository;
+    public StudentService(StudentRepository studentrepository) {
+        this.studentrepository = studentrepository;
+    }
+
 }
