6451ec52e07a7d2f45cec1bcd8dc45b597649906
flo7716 <florian.andre.77@gmail.com>
2025-04-07 16:22:13 +0200

    modified env import

--- NoSQL/weaviate_client.py
+++ NoSQL/weaviate_client.py
@@ -2,14 +2,23 @@ import os
 import weaviate
 from weaviate.classes.init import Auth
 
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
 # Best practice: store your credentials in environment variables
-weaviate_url = os.environ["WEAVIATE_URL"]
-weaviate_api_key = os.environ["WEAVIATE_API_KEY"]
+weaviate_url = os.getenv("WEAVIATE_URL")
+weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
 
 # Connect to Weaviate Cloud
-client = weaviate.connect_to_weaviate_cloud(
-    cluster_url=weaviate_url,
-    auth_credentials=Auth.api_key(weaviate_api_key),
-)
+try:
+    client = weaviate.connect_to_weaviate_cloud(
+        cluster_url=weaviate_url,
+        auth_credentials=Auth.api_key(weaviate_api_key),
+    )
 
-print(client.is_ready())
+    print(client.is_ready())
+    client.close()
+except Exception as e:
+    print("Error connecting to Weaviate Cloud:", e)
+
