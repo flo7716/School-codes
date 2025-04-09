87446d2e24f2372b3dc44b483eaab21ce9009505
flo7716 <florian.andre.77@gmail.com>
2025-04-09 09:08:34 +0200

    ajout import student

--- JEE/demo/src/main/java/com/example/demo/service/StudentService.java
+++ JEE/demo/src/main/java/com/example/demo/service/StudentService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
-import java.util.List;
 
 @Service
 
