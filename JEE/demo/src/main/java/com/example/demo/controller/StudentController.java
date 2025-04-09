fe2bf2ee20c75a9f293fb300f32e3eb6b9d96557
flo7716 <florian.andre.77@gmail.com>
2025-04-09 09:48:46 +0200

    ajout route home

---
+++ JEE/demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.service.StudentService;
+
+
+
+@Controller
+public class StudentController {
+    @Autowired
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "index";
+    }
+    
+
+}
