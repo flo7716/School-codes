5226b04d5f41350a1f4779ac6755fe6a939cf269
flo7716 <florian.andre.77@gmail.com>
2025-04-09 09:07:48 +0200

    ajout findbyname

--- JEE/demo/src/main/java/com/example/demo/service/StudentService.java
+++ JEE/demo/src/main/java/com/example/demo/service/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.StudentRepository;
+import java.util.List;
 
 @Service
 
@@ -12,6 +13,10 @@ public class StudentService {
     private final StudentRepository studentrepository;
     public StudentService(StudentRepository studentrepository) {
         this.studentrepository = studentrepository;
+    }
+
+    public List<Student> findByName(String name) {
+        return studentrepository.findByName(name);
     }
 
 }
