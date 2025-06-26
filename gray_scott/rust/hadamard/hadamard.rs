262f4adb72091c7a4fe8a2bcf6ef7553198f207e
flo7716 <florian.andre.77@gmail.com>
2025-06-26 11:44:11 +0200

    deleted assert eq

--- gray_scott/rust/hadamard/hadamard.rs
+++ gray_scott/rust/hadamard/hadamard.rs
@@ -4,14 +4,6 @@ fn hadamard(v1: Vec<f32>, v2: Vec<f32>) -> Vec<f32> {
                   .map(|(x1, x2)| x1 * x2)
                   .collect() 
 }
-assert_eq!(
-    hadamard(vec![1.2, 3.4, 5.6], vec![9.8, 7.6, 5.4]),
-    [
-        1.2 * 9.8,
-        3.4 * 7.6,
-        5.6 * 5.4
-    ]
-);
 
 fn main() {
     let v1 = vec![1.2, 3.4, 5.6];
