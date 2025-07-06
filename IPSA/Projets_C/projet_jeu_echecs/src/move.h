0022d1a436a98cdbde2693c5aad1352d80c5ae8d
flo7716 <florian.andre.77@gmail.com>
2025-07-06 21:23:30 +0200

    suppression jeu echecs

--- IPSA/Projets_C/projet_jeu_echecs/src/move.h
+++
@@ -1,7 +0,0 @@
-#ifndef MOVE_H
-#define MOVE_H
-
-int isValidMove(char board[8][8], int srcRow, int srcCol, int dstRow, int dstCol, int turn);
-void movePiece(char board[8][8], int srcRow, int srcCol, int dstRow, int dstCol);
-
-#endif
