7873338e9286bd5cf5f417fca419a80dd3366773
flo7716 <florian.andre.77@gmail.com>
2025-09-27 21:57:21 +0200

    modif saisie x et y

--- IPSA/Projets_C/gestion_etudiants/seance 1/exo2.c
+++ IPSA/Projets_C/gestion_etudiants/seance 1/exo2.c
@@ -3,8 +3,10 @@
 int main()
 {
     float x,y; //declaration des variables
-    printf("donner x et y:"); //saisie des variables
-    scanf("%f %f",&x,&y);
+    printf("donner x : "); //saisie des variables
+    scanf("%f",&x);
+    printf("donner y : ");
+    scanf("%f",&y);
     printf("la somme de %f et %f est egal a %f\n",x,y,x+y); //somme de x et y
     printf("le produit de %f et %f est egal a %f",x,y,x*y); //produit de x et y
     return 0;
