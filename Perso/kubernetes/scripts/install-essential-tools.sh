313b8ef87fed0fd49de679de50af1ed8a9fb4782
= <=>
2025-11-28 13:11:35 +0100

    iptables

--- Perso/kubernetes/scripts/install-essential-tools.sh
+++ Perso/kubernetes/scripts/install-essential-tools.sh
@@ -20,5 +20,5 @@ apt-get install -y \
 apt-get install -y procps
 
 #installation des dependances pour le reseau (bridge-utils et iptables)
-apt-get install -y bridge-utils iptables-nft
+apt-get install -y bridge-utils iptables
 echo "Essential tools have been installed successfully."
