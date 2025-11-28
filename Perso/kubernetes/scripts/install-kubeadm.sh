8ccb9cd144dac4ff4da9ffe2aa35ef7841b35f67
= <=>
2025-11-28 13:34:07 +0100

    ajout initialisation cluster

--- Perso/kubernetes/scripts/install-kubeadm.sh
+++ Perso/kubernetes/scripts/install-kubeadm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# ------------------------------
+# Install Kubernetes components
+# ------------------------------
+
 # Update package lists
 apt-get update
 
@@ -32,4 +36,26 @@ apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
 echo "Kubernetes components installed successfully."
-kubectl version
+kubectl version --client --short
+
+# ------------------------------
+# Initialize Kubernetes cluster
+# ------------------------------
+
+# Only run kubeadm init if not already initialized
+if [ ! -f /etc/kubernetes/admin.conf ]; then
+    echo "Initializing Kubernetes cluster..."
+    kubeadm init --pod-network-cidr=10.244.0.0/16
+
+    # Setup kubeconfig for vagrant user
+    mkdir -p /home/vagrant/.kube
+    cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+    chown vagrant:vagrant /home/vagrant/.kube/config
+
+    echo "Cluster initialized. kubeconfig copied to /home/vagrant/.kube/config"
+
+    # Apply Flannel CNI network
+    su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml"
+else
+    echo "Kubernetes cluster is already initialized."
+fi
