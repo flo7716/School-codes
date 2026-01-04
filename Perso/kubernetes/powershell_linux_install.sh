face584f1224e7c2d8dde5b4a0fb92d80dea1b40
flo7716 <florian.andre.77@gmail.com>
2026-01-04 10:46:57 +0100

    ajout installation powershell

---
+++ Perso/kubernetes/powershell_linux_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Download the powershell '.tar.gz' archive
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.5.4/powershell-7.5.4-linux-x64.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/7
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+# Set execute permissions
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
