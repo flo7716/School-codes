264ad31bc5662201f2533ab68bfa75002ed01381
flo7716 <florian.andre.77@gmail.com>
2026-01-16 00:18:10 +0100

    1er programme c#

---
+++ Perso/C#/Partie_1/variables/Program.cs
@@ -0,0 +1,8 @@
+//Declaration variable 
+string prenom = "Florian";
+string nom = "André";
+int age = 24;
+
+
+//TODO : Afficher dans la console "Bonjour, je m'appelle [prenom] [nom] et j'ai [age] ans."
+Console.WriteLine("Bonjour, je m'appelle " + prenom + " " + nom + " et j'ai " + age + " ans.");
