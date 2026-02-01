fbc7ef9b7d3fe9c7452975f80b9f291d60b93462
flo7716 <florian.andre.77@gmail.com>
2026-02-01 21:37:43 +0100

    ajout 10 etages

--- IPSA/parc_info_sql/modification.sql
+++ IPSA/parc_info_sql/modification.sql
@@ -5,6 +5,13 @@ SET etage =
         WHEN '130.120.80' THEN 0
         WHEN '130.120.81' THEN 1
         WHEN '130.120.82' THEN 2
+        WHEN '130.120.83' THEN 3
+        WHEN '130.120.84' THEN 4
+        WHEN '130.120.85' THEN 5
+        WHEN '130.120.86' THEN 6
+        WHEN '130.120.87' THEN 7
+        WHEN '130.120.88' THEN 8
+        WHEN '130.120.89' THEN 9
     END;
 
 
