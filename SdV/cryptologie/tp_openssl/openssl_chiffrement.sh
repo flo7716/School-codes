5e7ce44f3c65b551a81f24c8283b3b662b8d46ad
flo7716 <florian.andre.77@gmail.com>
2026-02-17 10:14:42 +0100

    reduction a 1 algo

--- SdV/cryptologie/tp_openssl/openssl_chiffrement.sh
+++ SdV/cryptologie/tp_openssl/openssl_chiffrement.sh
@@ -2,10 +2,6 @@
 
 # Chiffrement d'un fichier de clés RSA (DES, DES3 ou IDEA)
 openssl rsa -in private.pem -des3 -out private_des3.pem
-openssl rsa -in private.pem -des -out private_des.pem
-openssl rsa -in private.pem -idea -out private_idea.pem
 
 # Visualisation des clés RSA chiffrées
 cat private_des3.pem
-cat private_des.pem
-cat private_idea.pem
