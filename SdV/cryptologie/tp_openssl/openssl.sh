e1ec2bb2adde31903c8399f976d3b98b7e3061fc
flo7716 <florian.andre.77@gmail.com>
2026-02-17 10:05:17 +0100

    ajout chiffrement

--- SdV/cryptologie/tp_openssl/openssl.sh
+++ SdV/cryptologie/tp_openssl/openssl.sh
@@ -10,3 +10,14 @@ openssl genrsa -out private.pem 2048
 openssl rsa -in private.pem -text -noout
 
 
+# Chiffrement d'un fichier de clés RSA (DES, DES3 ou IDEA)
+openssl rsa -in private.pem -des3 -out private_des3.pem
+openssl rsa -in private.pem -des -out private_des.pem
+openssl rsa -in private.pem -idea -out private_idea.pem
+
+# Visualisation des clés RSA chiffrées
+cat private_des3.pem
+cat private_des.pem
+cat private_idea.pem
+
+
