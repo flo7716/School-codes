af9ff8547ef508e876ae1e067a2b42b561b3a6d1
flo7716 <florian.andre.77@gmail.com>
2026-03-03 17:40:06 +0100

    ajout script installation agent

---
+++ SdV/SIEM/wazuh/agent/debian_agent_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+
+# Install packages if missing
+apt-get install gnupg apt-transport-https -y
+
+# Install GPG key
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+
+# Add repository
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+
+# Check if the repository was added successfully
+apt-get update
