5b743e7a63a57e58401c8d4b902af775a444b68c
flo7716 <florian.andre.77@gmail.com>
2026-09-09 12:42:16 +0200

    fin tp null values

--- ECE/ING4/semestre_1/bases_donnees_avancees/tp1/null_values.sql
+++ ECE/ING4/semestre_1/bases_donnees_avancees/tp1/null_values.sql
@@ -22,3 +22,9 @@ SELECT ENAME, COALESCE(COMM, 0.0) / 1.2 AS COMM_EURO FROM EMP;
 -- Nom et salaire total (salaire + commission) de tous les employés, en considérant que les NULL sont des 0.0
 SELECT ENAME, SAL + COALESCE(COMM, 0.0) AS SAL_TOTAL FROM EMP;
 
+-- Nom des plus hauts responsables de l'entreprise (ceux qui n'ont pas de supérieur hiérarchique)
+SELECT ENAME FROM EMP WHERE MGR IS NULL;
+
+-- Employes dont la commission est inférieure à 25% de leur salaire (avec et sans NULL)
+SELECT * FROM EMP WHERE COMM < 0.25 * SAL;
+SELECT * FROM EMP WHERE COMM IS NOT NULL AND COMM < 0.25 * SAL;
