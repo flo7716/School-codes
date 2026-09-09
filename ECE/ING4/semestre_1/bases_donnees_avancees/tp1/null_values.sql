c463ad88f48454d5b30500ffa08e0c036efb1a10
flo7716 <florian.andre.77@gmail.com>
2026-09-09 12:36:59 +0200

    ajout requetes jusqu'au salaire total

--- ECE/ING4/semestre_1/bases_donnees_avancees/tp1/null_values.sql
+++ ECE/ING4/semestre_1/bases_donnees_avancees/tp1/null_values.sql
@@ -10,3 +10,15 @@ SELECT COUNT(COMM) FROM EMP WHERE COMM IS NOT NULL;
 -- Nombre d'employes dont la commission n'est pas specifiee en 2 methodes
 SELECT COUNT(*) FROM EMP WHERE COMM IS NULL;
 
+-- la commission la plus basse, la moyenne et la plus élevée (on écarte les NULL)
+SELECT MIN(COMM), AVG(COMM), MAX(COMM) FROM EMP WHERE COMM IS NOT NULL;
+
+-- Commission moyenne de tous les employés, en considérant que les NULL sont des 0.0
+SELECT AVG(COALESCE(COMM, 0.0)) FROM EMP;
+
+-- Nom et commission de tous les employés en euros (1€ = 1.2$)
+SELECT ENAME, COALESCE(COMM, 0.0) / 1.2 AS COMM_EURO FROM EMP;
+
+-- Nom et salaire total (salaire + commission) de tous les employés, en considérant que les NULL sont des 0.0
+SELECT ENAME, SAL + COALESCE(COMM, 0.0) AS SAL_TOTAL FROM EMP;
+
